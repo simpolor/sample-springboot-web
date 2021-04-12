7554c86dc0c01211d0f5e78be7f346809aa74aa3
sy.park <sy.park@powderroom.co.kr>
2021-04-12 19:56:59 +0900

    .

--- sample-springboot-pageable/src/main/java/io/simpolor/pageable/controller/StudentApiController.java
+++ sample-springboot-pageable/src/main/java/io/simpolor/pageable/controller/StudentApiController.java
@@ -5,8 +5,11 @@ import io.simpolor.pageable.domain.Student;
 import io.simpolor.pageable.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +30,10 @@ public class StudentApiController {
 	@RequestMapping(value="/api/v2/student/list", method=RequestMethod.GET)
 	public PageableResponse<Student> listV2(Pageable pageable) {
 
-		Page<Student> page = studentService.get(pageable);
+		Page<Student> students = studentService.get(pageable);
 
-		return PageableResponse.of(page.getContent(), page.getTotalPages(),
-				page.getTotalElements(), page.getNumber(), page.getSize());
+		return PageableResponse.of(students.getContent(), students.getTotalPages(),
+				students.getTotalElements(), students.getNumber(), students.getSize());
 	}
 
 	@RequestMapping(value="/api/v3/student/list", method=RequestMethod.GET)
@@ -40,41 +43,15 @@ public class StudentApiController {
 		return studentService.list(page, size);
 	}
 
-	/***
-	 * {
-	 *   "content": [
-	 *     {
-	 *       "seq": 1,
-	 *       "name": "하니",
-	 *       "grade": 1,
-	 *       "age": 17
-	 *     }
-	 *   ],
-	 *   "pageable": {
-	 *     "sort": {
-	 *       "sorted": false,
-	 *       "unsorted": true,
-	 *       "empty": true
-	 *     },
-	 *     "offset": 0,
-	 *     "pageSize": 10,
-	 *     "pageNumber": 0,
-	 *     "paged": true,
-	 *     "unpaged": false
-	 *   },
-	 *   "totalPages": 3,
-	 *   "totalElements": 26,
-	 *   "last": false,
-	 *   "number": 0,
-	 *   "size": 10,
-	 *   "sort": {
-	 *     "sorted": false,
-	 *     "unsorted": true,
-	 *     "empty": true
-	 *   },
-	 *   "numberOfElements": 10,
-	 *   "first": true,
-	 *   "empty": false
-	 * }
-	 */
+	@RequestMapping(value="/api/v4/student/list", method=RequestMethod.GET)
+	public PageableResponse<Student> listV4(@RequestParam(name="page", defaultValue = "1") int page,
+											@RequestParam(name="size", defaultValue = "10") int size,
+											@SortDefault(value = "id", direction = Sort.Direction.DESC) Sort sort) {
+
+		Pageable pageable = PageRequest.of(page, size, sort);
+		Page<Student> students = studentService.get(pageable);
+
+		return PageableResponse.of(students.getContent(), students.getTotalPages(),
+				students.getTotalElements(), students.getNumber(), students.getSize());
+	}
 }
