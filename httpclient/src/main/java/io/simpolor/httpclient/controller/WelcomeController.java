b40f7a59c5904128b87af58ff071f1a39664f83b
sy.park <sy.park@powderroom.co.kr>
2023-01-11 18:48:47 +0900

    .

--- httpclient/src/main/java/io/simpolor/httpclient/controller/WelcomeController.java
+++ httpclient/src/main/java/io/simpolor/httpclient/controller/WelcomeController.java
@@ -10,6 +10,6 @@ public class WelcomeController {
     @RequestMapping({"/", "/index", "/welcome"})
     @ResponseBody
     public String welcome() {
-        return "httpClient";
+        return "HttpClient";
     }
 }
