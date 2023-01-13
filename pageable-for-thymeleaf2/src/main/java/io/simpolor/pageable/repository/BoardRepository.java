f6a594447345801ffa3844e40b5b67f09d8064bc
sy.park <sy.park@powderroom.co.kr>
2023-01-13 17:34:26 +0900

    .

--- pageable-for-thymeleaf2/src/main/java/io/simpolor/pageable/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package io.simpolor.pageable.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import io.simpolor.pageable.repository.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
